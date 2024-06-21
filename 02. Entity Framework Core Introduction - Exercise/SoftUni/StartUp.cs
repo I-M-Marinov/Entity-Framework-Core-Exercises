eea2e3f9e8ea1a476920f37a639d3fb27673f56a
I-M-Marinov <lcfrrr@gmail.com>
2024-06-22 00:39:31 +0300

    Exercise 6 Adding a New Address and Updating Employee == SOLVED
    Exercise 6 Adding a New Address and Updating Employee == SOLVED

--- 02. Entity Framework Core Introduction - Exercise/SoftUni/StartUp.cs
+++ 02. Entity Framework Core Introduction - Exercise/SoftUni/StartUp.cs
@@ -8,7 +8,36 @@ namespace SoftUni
     public class StartUp
 
     {
+        public static string AddNewAddressToEmployee(SoftUniContext context)
+        {
+            var newAddress = new Address();
+            newAddress.AddressText = "Vitoshka 15";
+            newAddress.TownId = 4;
 
+            context.Addresses.Add(newAddress);
+            context.SaveChanges();
+
+            var employeeId = context.Employees
+                .Where(e => e.LastName == "Nakov")
+                .Select(e => e.EmployeeId)
+                .FirstOrDefault();
+
+            var employee = context.Employees.Find(employeeId);
+
+            employee.AddressId = newAddress.AddressId;
+
+            context.SaveChanges();
+
+
+            var addressTexts = context.Employees
+                .OrderByDescending(e => e.AddressId)
+                .Take(10)
+                .Select(e => e.Address.AddressText)
+                .ToList();
+
+            return string.Join(Environment.NewLine, addressTexts);
+            
+        }
         public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
         {
             var employees = context.Employees
@@ -73,7 +102,7 @@ namespace SoftUni
         public static void Main(string[] args)
         {
 
-            /* EXERCISE 3 BELOW */
+            /* EXERCISE 3  */
 
             //using (var context = new SoftUniContext())
             //{
@@ -82,7 +111,7 @@ namespace SoftUni
             //    Console.WriteLine(employeeInfo);
             //}
 
-            /* EXERCISE 4 BELOW */
+            /* EXERCISE 4  */
             //using (var context = new SoftUniContext())
             //{
             //    string info = GetEmployeesWithSalaryOver50000(context);
@@ -91,13 +120,21 @@ namespace SoftUni
             //}
 
 
-            /* EXERCISE 5 BELOW */
+            /* EXERCISE 5  */
+
+            //using (var context = new SoftUniContext())
+            //{
+            //    string info = GetEmployeesFromResearchAndDevelopment(context);
+
+            //    Console.WriteLine(info);
+            //}
+
+
+            /* EXERCISE 6  */
 
             using (var context = new SoftUniContext())
             {
-                string info = GetEmployeesFromResearchAndDevelopment(context);
-
-                Console.WriteLine(info);
+                Console.WriteLine(AddNewAddressToEmployee(context));
             }
 
         }

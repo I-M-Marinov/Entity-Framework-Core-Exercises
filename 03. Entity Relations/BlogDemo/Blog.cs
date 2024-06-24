140785dff20aebdede4cb3b04e796132dd1a11c1
I-M-Marinov <lcfrrr@gmail.com>
2024-06-24 23:09:39 +0300

    BlogDemo Project - Table Relations Exercise - Code First
    Create a Table with the Fluent API ( Class Blog )

---
+++ 03. Entity Relations/BlogDemo/Blog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogDemo
+{
+    public class Blog
+    {
+        public int BlogId { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string? Description { get; set; }
+    }
+}
---
+++ 03. Entity Relations/BlogDemo/BlogDbContext.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogDemo
+{
+    public class BlogDbContext : DbContext
+    {
+
+        public BlogDbContext()
+        {
+
+        }
+
+        public BlogDbContext(DbContextOptions<BlogDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Blog>()
+                .HasKey(b => b.BlogId);
+
+            modelBuilder.Entity<Blog>()
+                .ToTable("Blogs", "blg");
+
+            modelBuilder.Entity<Blog>()
+                .Property(b => b.Name)
+                .HasColumnName("BlogName")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(50)
+                .IsRequired();
+
+            modelBuilder.Entity<Blog>()
+                .Property(b => b.Description)
+                .HasColumnName("Description")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(500);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            if (optionsBuilder.IsConfigured == false)
+            {
+                string connectionString = "Server=MARINOV-GAME-PC\\SQLEXPRESS; Database = BlogDb; Integrated Security = true; Encrypt = False; TrustServerCertificate = true;";
+
+                    optionsBuilder.UseSqlServer(connectionString);
+
+            }
+
+
+        }
+    }
+}

bf774755f4ef073afc9f164622b4f633d31500be
I-M-Marinov <lcfrrr@gmail.com>
2024-06-26 15:01:19 +0300

    Added the DbContext, Team and Color to the Models folder
    Added the FootballBettingContext and
    - Team and Color to the Models folder
    - Added Team and Color to the Fluent API

---
+++ 04. Enttity Relations - Exercise/FootballBetting/P02_FootballBetting/Data/FootballBettingContext.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using P02_FootballBetting.Data.Models;
+
+namespace P02_FootballBetting.Data
+{
+    internal class FootballBettingContext: DbContext
+    {
+
+        public FootballBettingContext()
+        {
+
+        }
+
+        public FootballBettingContext(DbContextOptions<FootballBettingContext> options)
+            : base(options)
+        {
+
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            if (optionsBuilder.IsConfigured == false)
+            {
+                string connectionString = "Server=MARINOV-GAME-PC\\SQLEXPRESS; Database = BlogDb; Integrated Security = true; Encrypt = False; TrustServerCertificate = true;";
+
+                optionsBuilder.UseSqlServer(connectionString);
+
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            /* TEAM */
+
+            modelBuilder.Entity<Team>()
+            .HasKey(t => t.TeamId);
+
+            modelBuilder.Entity<Team>()
+                .Property(t => t.Name)
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(100)
+                .IsRequired();
+
+            modelBuilder.Entity<Team>()
+                .Property(t => t.LogoUrl)
+                .HasColumnType("NVARCHAR")
+                .IsRequired();
+
+            modelBuilder.Entity<Team>()
+                .Property(t => t.Initials)
+                .HasColumnType("CHAR")
+                .HasMaxLength(3)
+                .IsRequired();
+
+            modelBuilder.Entity<Team>()
+                .Property(t => t.Budget)
+                .HasColumnType("MONEY")
+                .IsRequired();
+
+            modelBuilder.Entity<Team>()
+                .Property(t => t.PrimaryKitColorId)
+                .HasColumnType("INT")
+                .IsRequired();
+
+            modelBuilder.Entity<Team>()
+                .Property(t => t.SecondaryKitColorId)
+                .HasColumnType("INT")
+                .IsRequired();
+
+            modelBuilder.Entity<Team>()
+                .Property(t => t.TownId)
+                .HasColumnType("INT")
+                .IsRequired();
+
+            /* COLOR */
+
+            modelBuilder.Entity<Color>()
+                .HasKey(t => t.ColorId);
+
+            modelBuilder.Entity<Color>()
+                .Property(t => t.Name)
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(30)
+                .IsRequired();
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+    }
+}
---
+++ 04. Enttity Relations - Exercise/FootballBetting/P02_FootballBetting/Data/Models/Team.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P02_FootballBetting.Data.Models
+{
+    public class Team
+    {
+        public int TeamId { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string LogoUrl { get; set; } = null!;
+
+        public string Initials { get; set; } = null!;
+
+        public decimal Budget { get; set; }
+        
+        public int PrimaryKitColorId { get; set; }
+
+        public int SecondaryKitColorId { get; set; }
+
+        public int TownId { get; set; }
+    }
+}

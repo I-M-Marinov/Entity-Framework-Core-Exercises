1cbe2c2ce4c929d5ecf7d484edd5f4086ea13c9e
I-M-Marinov <lcfrrr@gmail.com>
2024-06-28 13:34:44 +0300

    MusicHub Database with Data Annotation
    Implemented the MusicHub Database with Data Annotation ( attributes )

---
+++ 05. LINQ - Exercise/MusicHub/Data/Configuration.cs
@@ -0,0 +1,9 @@
+﻿namespace MusicHub.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString =
+            @"Server=MARINOV-GAME-PC\\SQLEXPRESS; Database = MusicHub; Integrated Security = true; Encrypt = False; TrustServerCertificate = true;";
+    }
+}
+
---
+++ 05. LINQ - Exercise/MusicHub/Data/Models/Album.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MusicHub.Validation;
+
+namespace MusicHub.Data.Models
+{
+    public class Album
+    {
+
+
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(ValidationConstants.AlbumNameMaxLength)]
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        [NotMapped] // do not map properties that are calculated due to performance issues and/or achieve better normalization
+        public decimal Price
+        {
+            get
+            {
+                return Songs.Sum(song => song.Price);
+            }
+        }
+
+        public int? ProducerId { get; set; } // nullable 
+
+        [ForeignKey(nameof(ProducerId))] 
+        public Producer Producer { get; set; } = null!;
+
+        public ICollection<Song> Songs { get; set; } = new List<Song>();
+    }
+}

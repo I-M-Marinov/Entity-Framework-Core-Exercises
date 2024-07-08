87634d6ec22420a8a75ffdb79cc0a4524d1532a1
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2024-07-08 18:52:37 +0300

    Exercise 1 Import Users == SOLVED
    Exercise 1 Import Users == SOLVED

---
+++ 07. JSON Processing - Exercise/ProductShop/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+
+
+        public static void Main()
+        {
+            using var db = new ProductShopContext();
+
+            string filePath =
+                "C:\\Users\\Ivan Marinov\\Desktop\\Exercise\\07.JSON-Processing-Exercises-ProductShop-6.0\\ProductShop\\Datasets\\users.json";
+            string jsonData = File.ReadAllText(filePath);
+            ImportUsers(db, jsonData);
+        }
+
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            var users = JsonConvert.DeserializeObject<List<User>>(inputJson);
+            context.Users.AddRange(users);
+            context.SaveChanges();
+            return $"Successfully imported {users.Count}";
+        }
+    }
+}

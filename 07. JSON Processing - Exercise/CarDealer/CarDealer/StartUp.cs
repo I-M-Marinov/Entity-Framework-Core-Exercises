f12641f326a3ee97e260ee1e96da6a36dc5dbc81
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2024-07-10 16:30:46 +0300

    Query 10. Import Parts == SOLVED
    Query 10. Import Parts == SOLVED

--- 07. JSON Processing - Exercise/CarDealer/CarDealer/StartUp.cs
+++ 07. JSON Processing - Exercise/CarDealer/CarDealer/StartUp.cs
@@ -14,10 +14,23 @@ namespace CarDealer
             string suppliersFilePath =
                 "C:\\Users\\Ivan Marinov\\Desktop\\Exercise\\07.JSON-Processing-Exercises-CarDealer-6.0\\CarDealer\\Datasets\\suppliers.json";
 
+            string partsFilePath =
+                "C:\\Users\\Ivan Marinov\\Desktop\\Exercise\\07.JSON-Processing-Exercises-CarDealer-6.0\\CarDealer\\Datasets\\parts.json";
+
+            string carsFilePath =
+                "C:\\Users\\Ivan Marinov\\Desktop\\Exercise\\07.JSON-Processing-Exercises-CarDealer-6.0\\CarDealer\\Datasets\\cars.json";
+
+
             string jsonDataSuppliers = File.ReadAllText(suppliersFilePath);
+            string jsonDataParts = File.ReadAllText(partsFilePath);
+            string jsonDataCars = File.ReadAllText(carsFilePath);
 
 
-            Console.WriteLine(ImportSuppliers(db, jsonDataSuppliers));
+         //   Console.WriteLine(ImportSuppliers(db, jsonDataSuppliers));
+
+         //   Console.WriteLine(ImportParts(db, jsonDataParts));
+
+            Console.WriteLine(ImportCars(db, jsonDataCars));
 
 
         }
@@ -32,5 +45,35 @@ namespace CarDealer
 
             return $"Successfully imported {suppliers.Count}.";
         }
+
+        // Query 10. Import Parts 
+
+        public static string ImportParts(CarDealerContext context, string inputJson)
+        {
+            var parts = JsonConvert.DeserializeObject<List<Part>>(inputJson);
+
+            var validSuppliersIds = context.Suppliers.Select(s => s.Id).ToList();
+
+            var validParts = parts.Where(p => validSuppliersIds.Contains(p.SupplierId)).ToList();
+
+            context.Parts.AddRange(validParts);
+            context.SaveChanges();
+
+            return $"Successfully imported {validParts.Count}.";
+        }
+
+        // Query 11. Import Cars
+
+        public static string ImportCars(CarDealerContext context, string inputJson)
+        {
+
+            var cars = JsonConvert.DeserializeObject<List<Car>>(inputJson);
+
+            context.Cars.AddRange(cars);
+            context.SaveChanges();
+
+            return $"Successfully imported {cars.Count}.";
+        }
+
     }
 }

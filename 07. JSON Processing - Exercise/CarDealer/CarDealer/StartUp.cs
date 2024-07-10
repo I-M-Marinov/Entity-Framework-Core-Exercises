cfd781a94c5bc7b67199ef55d18ec1f5bc676c80
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2024-07-10 15:26:21 +0300

    Query 9. Import Suppliers == SOLVED
    Query 9. Import Suppliers == SOLVED

---
+++ 07. JSON Processing - Exercise/CarDealer/CarDealer/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using CarDealer.Data;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+
+            using var db = new CarDealerContext();
+
+            string suppliersFilePath =
+                "C:\\Users\\Ivan Marinov\\Desktop\\Exercise\\07.JSON-Processing-Exercises-CarDealer-6.0\\CarDealer\\Datasets\\suppliers.json";
+
+            string jsonDataSuppliers = File.ReadAllText(suppliersFilePath);
+
+
+            Console.WriteLine(ImportSuppliers(db, jsonDataSuppliers));
+
+
+        }
+
+        // Query 9. Import Suppliers 
+
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            var suppliers = JsonConvert.DeserializeObject<List<Supplier>>(inputJson);
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Count}.";
+        }
+    }
+}

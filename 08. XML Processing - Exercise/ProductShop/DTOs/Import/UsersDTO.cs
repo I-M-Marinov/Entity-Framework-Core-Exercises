800cdf377e9308e0835fba3db69ac52316e81095
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2024-07-12 16:34:41 +0300

    Query 1. Import Users == SOLVED
    Query 1. Import Users == SOLVED

---
+++ 08. XML Processing - Exercise/ProductShop/DTOs/Import/UsersDTO.cs
@@ -0,0 +1,26 @@
+﻿using ProductShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ProductShop.DTOs.Import
+{
+    using System.Xml.Serialization;
+
+    [XmlType("User")]
+    public class UsersDTO
+    {
+        [XmlElement("firstName")]
+        public string FirstName { get; set; }
+
+        [XmlElement("lastName")]
+        public string LastName { get; set; }
+
+        [XmlElement("age")]
+        public int? Age { get; set; }
+    }
+}
+
---
+++ 08. XML Processing - Exercise/ProductShop/StartUp.cs
@@ -0,0 +1,49 @@
+﻿using ProductShop.Data;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using ProductShop.DTOs.Import;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ProductShopContext db = new ProductShopContext();
+
+            var xmlUsersFilePath = "C:\\Users\\Ivan Marinov\\Desktop\\XML Exercise\\08.XML-Processing-Exercises-ProductShop-6.0\\ProductShop\\Datasets\\users.xml";
+            string inputXmlUsers = File.ReadAllText(xmlUsersFilePath);
+
+
+
+            Console.WriteLine(ImportUsers(db, inputXmlUsers));
+        }
+
+        // Query 1. Import Users
+        public static string ImportUsers(ProductShopContext context, string inputXml)
+        {
+         
+            XmlSerializer serializer = new XmlSerializer(typeof(UsersDTO[]), 
+                new XmlRootAttribute("Users"));
+
+            using StringReader stringReader = new StringReader(inputXml);
+
+            UsersDTO[] users = (UsersDTO[])serializer.Deserialize(stringReader);
+
+            List<User> usersToAdd = users
+                .Select(u => new User
+                {
+                    FirstName = u.FirstName,
+                    LastName = u.LastName,
+                    Age = u.Age
+                })
+                .ToList();
+
+            context.Users.AddRange(usersToAdd);
+            context.SaveChanges();
+
+            return $"Successfully imported {usersToAdd.Count}.";
+        }
+    }
+}

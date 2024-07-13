97ac20902b852051f0df49e1db2591296857ead1
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2024-07-13 19:02:51 +0300

    Query 9. Import Suppliers == SOLVED
    Query 9. Import Suppliers == SOLVED

---
+++ 08. XML Processing - Exercise/CarDealer/CarDealer/DTOs/Import/SuppliersDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealer.DTOs.Import
+{
+    using System.Xml.Serialization;
+
+    [XmlType("Supplier")]
+    public class SuppliersDto
+    {
+        [XmlElement("name")] 
+        public string Name { get; set; } = null!;
+
+        [XmlElement("isImporter")] 
+        public bool IsImporter { get; set; }
+    }
+}
---
+++ 08. XML Processing - Exercise/CarDealer/CarDealer/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using CarDealer.Data;
+using CarDealer.Models;
+using System.Xml.Serialization;
+using CarDealer.DTOs.Import;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext db = new();
+
+            var xmlSuppliersFilePath = "C:\\Users\\Ivan Marinov\\Desktop\\XML Exercise\\08.XML-Processing-Exercises-CarDealer-6.0\\CarDealer\\Datasets\\suppliers.xml";
+            string inputXmlSuppliers = File.ReadAllText(xmlSuppliersFilePath);
+
+            Console.WriteLine(ImportSuppliers(db, inputXmlSuppliers));
+        }
+
+        // Query 9. Import Suppliers
+
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(SuppliersDto[]),
+                new XmlRootAttribute("Suppliers"));
+
+            using StringReader stringReader = new StringReader(inputXml);
+
+            SuppliersDto[] suppliers = (SuppliersDto[])serializer.Deserialize(stringReader);
+
+            List<Supplier> suppliersToAdd = suppliers
+                .Select(s => new Supplier()
+                {
+                    Name = s.Name,
+                    IsImporter = s.IsImporter
+                    
+                })
+                .ToList();
+
+            context.Suppliers.AddRange(suppliersToAdd);
+            context.SaveChanges();
+
+
+            return $"Successfully imported {suppliersToAdd.Count}";
+        }
+    }
+}

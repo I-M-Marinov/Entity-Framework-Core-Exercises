e4c9638e2f4fda2e68cf4595f45ee6cade9b92b7
I-M-Marinov <lcfrrr@gmail.com>
2024-07-30 16:27:44 +0300

    Import Data - XML Import - Import Plays
    Import Data - XML Import - Import Plays

--- 13. Exam Preparation/08. Exam Preparation - 04 Dec 2021/Theatre/DataProcessor/Deserializer.cs
+++ 13. Exam Preparation/08. Exam Preparation - 04 Dec 2021/Theatre/DataProcessor/Deserializer.cs
@@ -1,4 +1,10 @@
-﻿namespace Theatre.DataProcessor
+﻿using System.Text;
+using Theatre.Data.Models;
+using Theatre.Data.Models.Enums;
+using Theatre.DataProcessor.ImportDto;
+using Theatre.Utilities;
+
+namespace Theatre.DataProcessor
 {
     using System.ComponentModel.DataAnnotations;
 
@@ -21,7 +27,70 @@
 
         public static string ImportPlays(TheatreContext context, string xmlString)
         {
-            throw new NotImplementedException();
+            StringBuilder sb = new StringBuilder();
+
+            XmlHelper xmlHelper = new XmlHelper();
+            const string xmlRoot = "Plays";
+
+            ICollection<Play> playsToImport = new List<Play>();
+
+            ImportPlaysDto[] deserializedPlays = xmlHelper.Deserialize<ImportPlaysDto[]>(xmlString, xmlRoot);
+
+            foreach (ImportPlaysDto playDto in deserializedPlays)
+            {
+                var IsItValidDuration = TimeSpan.TryParse(playDto.Duration, out TimeSpan duration);
+
+                if (!IsValid(playDto) || !IsItValidDuration)
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                Genre genre;
+
+                if (playDto.Genre == "Drama")
+                {
+                    genre = Genre.Drama;
+                }
+                else if (playDto.Genre == "Comedy")
+                {
+                    genre = Genre.Comedy;
+                }
+                else if (playDto.Genre == "Romance")
+                {
+                    genre = Genre.Romance;
+                }
+                else if (playDto.Genre == "Musical")
+                {
+                    genre = Genre.Musical;
+                }
+                else
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+
+                Play newPlay = new Play()
+                {
+                    Title = playDto.Title,
+                    Duration = duration,
+                    Rating = (float)playDto.Raiting,
+                    Genre = genre,
+                    Description = playDto.Description,
+                    Screenwriter = playDto.Screenwriter
+                };
+
+                playsToImport.Add(newPlay);
+                sb.AppendLine(string.Format(SuccessfulImportPlay, newPlay.Title, newPlay.Genre.ToString(), newPlay.Rating));
+
+            }
+
+            context.Plays.AddRange(playsToImport);
+            context.SaveChanges();
+
+            return sb.ToString();
+
         }
 
         public static string ImportCasts(TheatreContext context, string xmlString)
---
+++ 13. Exam Preparation/08. Exam Preparation - 04 Dec 2021/Theatre/DataProcessor/ImportDto/ImportPlaysDto.cs
@@ -0,0 +1,46 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+using Theatre.Data.Models;
+using Theatre.Data.Models.Enums;
+using static Theatre.DataConstraints;
+
+
+namespace Theatre.DataProcessor.ImportDto
+{
+    [XmlType(nameof(Play))]
+    public class ImportPlaysDto
+    {
+        [XmlElement(nameof(Title))]
+        [MinLength(PlayTitleMinLength)]
+        [MaxLength(PlayTitleMaxLength)]
+        public string Title { get; set; } = null!;
+
+        [XmlElement(nameof(Duration))]
+        [Required]
+        [Range(typeof(TimeSpan), "01:00:00", "23:59:59")]
+        public string Duration { get; set; } = null!;
+
+        [XmlElement(nameof(Raiting))]
+        [Required]
+        [Range(PlayRatingMinLength, PlayRatingMaxLength)]
+
+        public double Raiting { get; set; }
+
+        [XmlElement(nameof(Genre))] 
+        [Required] 
+        public string Genre { get; set; } = null!;
+
+        [XmlElement(nameof(Description))]
+        [Required]
+        [StringLength(PlayDescriptionMaxLength)]
+        public string Description { get; set; } = null!;
+
+
+        [XmlElement(nameof(Screenwriter))]
+        [Required]
+        [MaxLength(PlayScreenwriterMaxLength)]
+
+        public string Screenwriter { get; set; } = null!;
+    }
+}

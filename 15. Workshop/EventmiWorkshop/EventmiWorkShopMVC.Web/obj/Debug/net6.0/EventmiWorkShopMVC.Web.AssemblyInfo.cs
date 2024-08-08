4c24c85066b58a548be1790036d5bfa6bc5abec1
I-M-Marinov <lcfrrr@gmail.com>
2024-08-08 12:56:32 +0300

    Updated the layout for the nav bar
    Removed the Edit and Remove dropdown items from the nav bar and renamed the "Add" to "Add an Event" as well as  "All" to "View all events"

--- 15. Workshop/EventmiWorkshop/EventmiWorkShopMVC.Web/obj/Debug/net6.0/EventmiWorkShopMVC.Web.AssemblyInfo.cs
+++ 15. Workshop/EventmiWorkshop/EventmiWorkShopMVC.Web/obj/Debug/net6.0/EventmiWorkShopMVC.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventmiWorkshopMVC.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b464b539e326c56b77a240adda2ad24d436c8e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+326a1a52874af886dcbbe52be792b401343e5d5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventmiWorkshopMVC.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventmiWorkshopMVC.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
